2a0d7c04e83109fd741bfbbcb0417c4c6a2e6469
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-15 22:07:18 +0530

    Standard Implementations for binary trees

---
+++ Trees/binarytree.cpp
@@ -0,0 +1,72 @@
+
+/**
+ * Some standard methods for Binary Tree.
+ */
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class BinaryNode{
+public:
+    BinaryNode(int val)
+    {
+        this->val = val;
+    }
+    BinaryNode* left;
+    BinaryNode* right;
+    int val;
+};
+
+// Level order traversal.
+void printBinaryTree(BinaryNode* root)
+{
+    if (!root)
+        return;
+    queue<BinaryNode*> q;
+    q.push(root);
+    while(!q.empty())
+    {   
+        int q_size = q.size();
+        for (int i=0;i<q_size;i++)
+        {
+            BinaryNode* currNode = q.front();
+            q.pop();
+            if (currNode)
+            {
+                cout << currNode->val << " ";
+                // pushing in nulls as well (if exist)
+                q.push(currNode->left);
+                q.push(currNode->right);
+            }
+            else {
+                cout << -1 << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+BinaryNode* constructBinaryTreeUtil(vector<int>& nodes, int index)
+{
+    if (index >= nodes.size() || nodes[index] == -1)
+        return NULL;
+    BinaryNode* newNode = new BinaryNode(nodes[index]);
+    newNode->left = constructBinaryTreeUtil(nodes, index*2 + 1);
+    newNode->right = constructBinaryTreeUtil(nodes, index*2 + 2);
+    return newNode;
+}
+
+// Pass into this perfect binary tree vector. -1 for null.
+BinaryNode* constructBinaryTree(vector<int>& nodes)
+{
+    BinaryNode* root = constructBinaryTreeUtil(nodes, 0);
+    return root;
+}
+
+int main()
+{
+    vector<int> nodes = {1, 2, 3, 6, 7, -1, 8};
+    printBinaryTree(constructBinaryTree(nodes));  
+}
