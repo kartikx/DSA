af6250bc6ce948e0b1c9b33ecb8646d980e8e592
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-25 13:07:41 +0530

    Optimized Insertion Sort by Using Assignment instead of Swapping

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insertionSort(vector<int>& v) {
+void insertionSortOrg(vector<int>& v) {
     for (int j=1; j<v.size(); j++) {
         int key = v[j];
         int i = j;
@@ -13,12 +13,19 @@ void insertionSort(vector<int>& v) {
     }
 }
 
-int main() {
-    vector<int> v{5, 2, 4, 6, 1, 3};
-    insertionSort(v);
-
-    for (auto x : v) {
-        cout << x << " ";
+void insertionSortOpt(vector<int>& v) {
+    for (int i=1; i<v.size(); i++) {
+        int key = v[i];
+        int j=i-1;
+        while (j >= 0 && v[j] > key) {
+            v[j+1] = v[j];
+            j--;
+        }
+        v[j+1] = key;
     }
-    cout << endl;
+}
+
+int main() {
+    vector<int> v{2, 6, 1, 1, 8 ,1, 2, 1, 5 ,22, 61, 7, 24, 9};
+    insetionSortOpt(v);
 }
